3572108fdc29813b3eca324cf60b71849a5fd643
Ionel Condor <icondor@sdl.com>
2016-05-28 12:24:15 +0300

    add demo for read in a java bean

---
+++ src/Agenda.java
@@ -0,0 +1,25 @@
+/**
+ * Created by icondor on 28/05/16.
+ */
+public class Agenda {
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    private String nume;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+
+}
